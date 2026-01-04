e22a9592b31f4fe91d536f8fd8ab9ca5d4c4e573
Adam <adamkhartouf@gmail.com>
2026-01-04 02:24:59 +0100

    vercel configur

--- to_do_app_project/settings.py
+++ to_do_app_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&58yz!04i_-#2*8=fn3xgd9n5j@dptg6_jtc&$r-1_&kv)8@kx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vercel.app']
+ALLOWED_HOSTS = ['vercel.app' , 'esma-django-to-do-app.vercel.app']
 
 
 # Application definition
